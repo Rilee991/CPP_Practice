940a89a160662dcd01ffff997c928641e14b40ed
cypher33 <rohitkumar25.rkl@gmail.com>
2024-01-02 16:18:29 +0530

    configurable dices

--- snakesandladders/main.cpp
+++ snakesandladders/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int boardSize = 100;
     vector<Player*> players = {new Player("Rohit"), new Player("Mohit")}; 
-    Dice* dice = new Dice(6, 1);
+    Dice* dice = new Dice(6, 3);
     vector<Entity*> entity = {new Entity(10, 15), new Entity(22, 66), new Entity(99, 4), new Entity(45, 71)};
  
     GameBoard* gb = new GameBoard(boardSize, players, dice, entity);
