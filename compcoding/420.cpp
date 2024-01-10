208dc4ed978e559b9e798d41f912857750fcaf58
cypher33 <rohitkumar25.rkl@gmail.com>
2024-01-10 12:58:58 +0530

    comp coding

---
+++ compcoding/420.cpp
@@ -0,0 +1,42 @@
+// C++ program of above approach
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+// A utility function to get max
+// of two integers
+int max(int x, int y) { return (x > y) ? x : y; }
+
+// Returns the length of the longest
+// palindromic subsequence in seq
+int lps(string seq, int i, int j) {
+    string rev = seq;
+    reverse(rev.begin(),rev.end());
+    int n = j+1;
+
+    int dp[n+1][n+1];
+
+    for(int a=0;a<=n;a++) {
+        for(int b=0;b<=n;b++) {
+            if(a == 0 || b == 0)
+                dp[a][b] = 0;
+            else if(seq[a-1] == rev[b-1])
+                dp[a][b] = 1 + dp[a-1][b-1];
+            else
+                dp[a][b] = max(dp[a-1][b],dp[a][b-1]);
+        }
+    }
+
+    return dp[n][n];
+}
+
+// Driver program to test above functions
+int main()
+{
+	string seq = "GEEKSFORGEEKS";
+	int n = seq.size();
+	cout << "The length of the LPS is "
+		<< lps(seq, 0, n - 1);
+	return 0;
+}
